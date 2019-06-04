43f602271c37f3dae8c3811413a0537aba03dae3
vaibhav.kumar <vaibhav.kumar.contractor@bbva.com>
2019-06-04 13:45:59 +0530

    added module3 code

--- src/app.js
+++ src/app.js
@@ -12,8 +12,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'UTF8' });
+const accountData = fs.readFileSync(path.join(__dirname, "json/accounts.json"), { encoding: 'UTF8' });
 const accounts = JSON.parse(accountData);
+// console.log(JSON.parse(accountData));
 
 const userData = fs.readFileSync('src/json/users.json', { encoding: 'UTF8' });
 const users = JSON.parse(userData);
@@ -38,7 +39,38 @@ app.get('/profile', (req, res) => {
     res.render('profile', { user: users[0] });
 });
 
+app.get('/transfer', (req, res) => {
+    res.render('transfer');
+});
+
+app.post('/transfer', (req, res) => {
+    let from = req.body.from;
+    let to = req.body.to;
+    console.log(req.body);
+    let amount = parseInt(req.body.amount);
+    accounts[from].balance = accounts[from].balance - amount;
+    accounts[to].balance = parseInt(accounts[to].balance) + amount;
+    const accountsJSON = JSON.stringify(accounts);
+    // const writePath = path.join(__dirname, "json/accounts.json");
+    console.log(path.join(__dirname, "json/accounts.json"));
+    fs.writeFileSync(path.join(__dirname, "json/accounts.json"), { accountsJSON }, { encoding: 'utf8' });
+    res.render('transfer', { message: "Transfer Completed" });
+});
+
+app.get('/payment', (req, res) => {
+    res.render('payment', { account: accounts.credit });
+});
+
+app.post('/payment', (req, res) => {
+    accounts.credit.balance = req.body.amount - accounts.credit.balance;
+    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
+    const accountsJSON = JSON.stringify(accounts);
+    // const writePath = path.join(__dirname, "json/accounts.json");
+    fs.writeFileSync(path.join(__dirname, "json/payments.json"), { accountsJSON }, { encoding: 'utf8' });
+    res.render('payment', { message: "Payment Successful", account: accounts.credit });
+});
+
 app.listen(3000, () => {
     console.log('server running');
 
-})
+});
