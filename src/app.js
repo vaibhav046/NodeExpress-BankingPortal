f20b3580c295bcd572745a5e3a7c40ec01203b09
vaibhav.kumar <vaibhav.kumar.contractor@bbva.com>
2019-06-06 12:48:45 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -12,11 +12,10 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-const accountData = fs.readFileSync(path.join(__dirname, "/json/accounts.json"), { encoding: 'UTF8' });
+const accountData = fs.readFileSync(path.join(__dirname, "/json/accounts.json"), 'UTF8');
 const accounts = JSON.parse(accountData);
-// console.log(JSON.parse(accountData));
 
-const userData = fs.readFileSync('src/json/users.json', { encoding: 'UTF8' });
+const userData = fs.readFileSync(path.join(__dirname, '/json/users.json'), 'UTF8');
 const users = JSON.parse(userData);
 
 app.get('/', (req, res) => {
