b6faf32dd616f9ca1622b532197a69bbf271b1d9
vaibhav.kumar <vaibhav.kumar.contractor@bbva.com>
2019-06-07 11:07:33 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
